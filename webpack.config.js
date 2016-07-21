265359960a50a1682e776e903fe77a1f9a519d9c
Gabin <hello@gabinaureche.com>
2016-07-22 00:10:19 +0200

    add css modules loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
       }
     ]
   }
