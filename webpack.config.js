eb7a814b1e14e756beed45d2a9288d776d5fbc1a
Gabin <hello@gabinaureche.com>
2016-07-23 18:00:54 +0200

    add postcss to transpile cssnext

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,9 @@ module.exports = {
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
       }
     ]
-  }
+  },
+  postcss: [
+    require('postcss-import')({ path: ['src']}),
+    require('postcss-cssnext')
+  ]
 };
