44b1e02700d8c3f9f4e30d021b9ac9ededa17e48
Gabin <hello@gabinaureche.com>
2016-07-23 23:02:12 +0200

    build and display tweets queue

--- src/components/Queue/index.js
+++ src/components/Queue/index.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './styles.css';
 import reduce from 'lodash/reduce';
+import values from 'lodash/values';
+import times from 'lodash/times';
 
 export default class Queue extends Component {
   static propTypes = {
@@ -9,7 +11,73 @@ export default class Queue extends Component {
 
   getOrderedList = () => {
     const { tweets } = this.props;
-    console.log(tweets);
+
+    // turns a complex map into an array of arrays, e.g:
+    // {
+    //   abc: {
+    //     0: {...},
+    //     1: {...}
+    //   },
+    //   def: {
+    //     0: {...}
+    //   }
+    // }
+    //
+    // becomes:
+    //
+    // [
+    //   [
+    //     {...},
+    //     {...}
+    //   ],
+    //   [
+    //     {...}
+    //   ]
+    // ]
+    const lists = reduce(tweets, (result, items) => {
+      result.push(values(items));
+      return result;
+    }, []);
+
+    // get the total number of tweets created
+    const total = reduce(lists, (result, list) => result + list.length, 0);
+
+    let sorted = [];
+    let currentListIndex = 0;
+
+    const nextListIndex = () => {
+      if (currentListIndex + 1 < lists.length) {
+        currentListIndex++;
+      } else {
+        currentListIndex = 0;
+      }
+    };
+
+    // we know that we have `total` tweets so that's the number of
+    // times we want to look for an one
+    times(total, () => {
+      // we may have lists with different lengths
+      // that's why for each iteration we need to
+      // find which list still has an item to be added
+      (function findItem () {
+        const currentList = lists[currentListIndex];
+
+        if (currentList.length) {
+          // this list still have some items so pull it out
+          sorted = sorted.concat(currentList.splice(0, 1));
+          return;
+        }
+
+        // oops, the list is empty so we'll
+        // need to look in the next one
+        nextListIndex();
+        findItem();
+      })();
+
+      nextListIndex();
+    });
+
+    return sorted;
   };
 
   render () {
@@ -22,8 +90,26 @@ export default class Queue extends Component {
 
           <p>
             That's the final order that's going to be sent to your Buffer queue.
-            You can also add tweets that may not be related to your Pocket's saved items.
           </p>
+
+          {this.getOrderedList().map((tweet, index) => (
+            <div key={index} className={styles.tweet}>
+              <div className={styles.tweetContent}>
+                {tweet.content}
+              </div>
+
+              {tweet.image ? (
+                <div
+                  className={styles.tweetImage}
+                  style={{ backgroundImage: `url('${tweet.image}')` }}
+                ></div>
+              ) : null}
+            </div>
+          ))}
+
+          <button type="button" className={styles.button}>
+            Add to Buffer
+          </button>
         </div>
       </div>
     );
